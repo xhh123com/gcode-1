2ed5eeb129c8686354f365c61a4718278f0e396d
15840345959@139.com <15840345959@139.com>
2019-08-18 20:23:48 +0800

    优化业务

--- resources/views/admin/web_route.blade.php
+++ resources/views/admin/web_route.blade.php
@@ -8,7 +8,7 @@
     Route::post('/{{$route_param_item['var_name']}}/edit', 'Admin\{{$route_param_item['model_name']}}Controller@editPost');  //编辑{{$route_param_item['var_name']}}
     Route::get('/{{$route_param_item['var_name']}}/setStatus/{id}', 'Admin\{{$route_param_item['model_name']}}Controller@setStatus');  //设置{{$route_param_item['var_name']}}状态
     Route::post('/{{$route_param_item['var_name']}}/deleteById/{id}', 'Admin\{{$route_param_item['model_name']}}Controller@deleteById');  //删除{{$route_param_item['var_name']}}
-
+    Route::get('/{{$route_param_item['var_name']}}/info', 'Admin\{{$route_param_item['model_name']}}Controller@info');  //详细细腻{{$route_param_item['var_name']}}
 
 @endforeach
 
