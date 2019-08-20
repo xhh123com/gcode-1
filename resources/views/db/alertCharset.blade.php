c5b299705709b58b2d9c306346cc67e303da753a
15840345959@139.com <15840345959@139.com>
2019-08-20 11:39:18 +0800

    生成业务

--- resources/views/db/alertCharset.blade.php
+++ resources/views/db/alertCharset.blade.php
@@ -8,16 +8,12 @@
     ALTER DATABASE {{env('DB_DATABASE','')}} CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
 
 @foreach($table_names as $table_name)
+
     ALTER TABLE `{{$table_name}}` CONVERT TO CHARACTER SET `utf8mb4` COLLATE `utf8mb4_general_ci`;
-@endforeach
-
-@foreach($table_names as $table_name)
     ALTER TABLE `{{$table_name}}` AUTO_INCREMENT=10000000;
-@endforeach
-
-@foreach($table_names as $table_name)
     ALTER TABLE `{{$table_name}}` ADD INDEX idx_seq (seq);
     ALTER TABLE `{{$table_name}}` ADD INDEX idx_status (status);
+
 @endforeach
 
 </html>
